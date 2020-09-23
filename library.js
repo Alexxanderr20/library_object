5dcbf93ea5e43131fceb1ec6e7800d1bf0bdd51e
Edward Munoz <alexmu419@icloud.com>
2020-09-23 02:40:35 -0500

    adding default books

--- library.js
+++ library.js
@@ -2,43 +2,34 @@ const bookCard = document.getElementById("book-container");
 const bookButton = document.getElementById("book-button");
 
 
-let myNewLibrary =
-[
-    //  bookOne = new bookProp("Harry Potter", "J.K. Rowling" , 624, true),
-    //  bookTwo = new bookProp("It", "Stephen King" , 912, true),
-    //  bookThree = new bookProp("The Da Vinci Code", "Dan Brown" , 200, true)
+let myNewLibrary = [];
 
-];
-
-// class bookProp
-// {
-//      constructor(title, author, numOfPages, haveRead)
-//     {
-//         this.title = title;
-//         this.author = author;
-//         this.numOfPages = numOfPages;
-//         this.read = haveRead;
-//     }
-// }
-
-function addNewBook()
+function defaultBooks()
 {
-    let user = {};
+    var bookOne = new book("Harry Potter", "J.K. Rowling" , 624, true);
+    var bookTwo = new book("It", "Stephen King" , 912, true);
+    var bookThree = new book("The Da Vinci Code", "Dan Brown" , 200, true);
+
+    console.log(bookOne);
+    console.log(bookTwo);
+    console.log(bookThree);
 }
 
-function addNewBookToLibrary()
-{
-    for(var i = 0; i < myNewLibrary.length; i++)
+     function book(title, author, numOfPages, haveRead)
     {
-        console.log(myNewLibrary[i].title);
-        console.log(myNewLibrary[i].author);
-        console.log(myNewLibrary[i].numOfPages);
-        console.log(myNewLibrary[i].haveRead);
-
-        var printArray = myNewLibrary.values(bookOne);
-        document.getElementById("demo").innerHTML = printArray;
+        this.title = title;
+        this.author = author;
+        this.numOfPages = numOfPages;
+        this.read = haveRead;
     }
+
+function addNewBookToLibrary(name, author, pages, readStatus)
+{
+    let books = new book(name,author, pages, readStatus);
+    myNewLibrary.push(books);
 }
 
 
-displayBook();
+// displayBook();
+
+defaultBooks();
