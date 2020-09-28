41a507cd5ed88e271780f8e9e4bfecd31280d8f2
Edward Munoz <alexmu419@icloud.com>
2020-09-28 02:25:51 -0500

    created clear function

--- library.js
+++ library.js
@@ -1,19 +1,27 @@
+//Adding DOM Selectors for User interaction
 const bookCard = document.getElementById("book-container");
 const bookButton = document.getElementById("book-button");
+const addButton = document.getElementById("add-book-button");
+const clearButton = document.getElementById("clear-book-button");
+const titleInput = document.getElementById("title-text");
+const authorInput = document.getElementById("author-text");
+const pagesInput = document.getElementById("pages-text");
+//const readInput = document.getElementById("")
 
 
+//creating a empty array to hold books
 let myNewLibrary = [];
 
-function defaultBooks()
-{
-    var bookOne = new book("Harry Potter", "J.K. Rowling" , 624, true);
-    var bookTwo = new book("It", "Stephen King" , 912, true);
-    var bookThree = new book("The Da Vinci Code", "Dan Brown" , 200, true);
+// function defaultBooks()
+// {
+//     var bookOne = new book("Harry Potter", "J.K. Rowling" , 624, true);
+//     var bookTwo = new book("It", "Stephen King" , 912, true);
+//     var bookThree = new book("The Da Vinci Code", "Dan Brown" , 200, true);
 
-    console.log(bookOne);
-    console.log(bookTwo);
-    console.log(bookThree);
-}
+//     console.log(bookOne);
+//     console.log(bookTwo);
+//     console.log(bookThree);
+// }
 
      function book(title, author, numOfPages, haveRead)
     {
@@ -29,6 +37,11 @@ function addNewBookToLibrary(name, author, pages, readStatus)
     myNewLibrary.push(books);
 }
 
+function clearBooks()
+{
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
 
 // displayBook();
 
