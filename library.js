56785283ed17226de6732dd62720ccea233f77ff
Edward Munoz <alexmu419@icloud.com>
2020-09-29 21:00:45 -0500

    created new book construct

--- library.js
+++ library.js
@@ -1,52 +1,23 @@
 //Adding DOM Selectors for User interaction
-const bookCard = document.getElementById("book-container");
-const bookButton = document.getElementById("book-button");
-const addButton = document.getElementById("add-book-button");
-const clearButton = document.getElementById("clear-book-button");
-const titleInput = document.getElementById("title-text");
-const authorInput = document.getElementById("author-text");
-const pagesInput = document.getElementById("pages-text");
-//const readInput = document.getElementById("")
+const bookCard = document.getElementById("#book-container");
+const bookButton = document.getElementById("#book-button");
+const addButton = document.getElementById("#add-book-button");
+const deleteButton = document.getElementById("#delete-book-button");
+const titleInput = document.getElementById("#title-text");
+const authorInput = document.getElementById("#author-text");
+const pagesInput = document.getElementById("#pages-text");
 
-//Creating Event Listeners
-clearButton.addEventListener("click", clearButton);
 //creating a empty array to hold books
 let myNewLibrary = [];
 
-     function book(title, author, numOfPages, haveRead)
-    {
-        this.title = title;
-        this.author = author;
-        this.numOfPages = numOfPages;
-        this.read = haveRead;
-    }
-
-    function getNewBook()
-    {
-        const bookTitle = titleInput.value;
-        const authorName = authorInput.value;
-        const pageNumber = pagesInput.value;
-        myNewLibrary.push(new book(title, author, numOfPages, haveRead));
-    }
-
-// function addNewBookToLibrary()
-// {
-//     let books = new book(name,author, pages, readStatus);
-//     myNewLibrary.push(books);
-// }
-
-function clearBooks()
+class Book
 {
-    titleInput.value = "";
-    authorInput.value = "";
-    pagesInput.value = "";
-}
-// displayBook();
-
-
-function addNewBook()
-{
-
+    constructor(title, author, numOfPages, haveRead)
+        {
+            this.title = title,
+            this.author = author,
+            this.pages = numOfPages,
+            this.read = haveRead;
+        }
 }
 
-clearBooks();
